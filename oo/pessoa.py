4918d0c37031b207773b31be4a3d23efd08f006e
mariliamps <marilia.ps@gmail.com>
2020-07-17 16:46:58 -0300

    criado atributos e instancias

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=36):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__=='__main__':
-    p = Pessoa()
+    p = Pessoa('Jose')
     print(id(p))
     print (p.cumprimentar())
+    print (p.nome)
+    p.nome='Marilia'
+    print(p.nome)
+    print(p.idade)
