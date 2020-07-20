e44f510b99ffd3f209102da5dc4bac183b057ca1
mariliamps <marilia.ps@gmail.com>
2020-07-20 09:57:30 -0300

    criado atributos complexos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=36):
+    def __init__(self, *filhos, nome=None, idade=36):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__=='__main__':
-    p = Pessoa('Jose')
-    print(id(p))
-    print (p.cumprimentar())
-    print (p.nome)
-    p.nome='Marilia'
-    print(p.nome)
-    print(p.idade)
+    marilia = Pessoa(nome='Marilia', idade=12)
+    jose = Pessoa(marilia, nome='Jose2', idade=37)
+    print(Pessoa.cumprimentar(jose))
+    #print (jose.cumprimentar())
+    print(jose.nome)
+    print(jose.idade)
+    for filho in jose.filhos:
+        print(filho.nome)
+        print(filho.idade)
+    jose.sobrenome = 'Silva'
+    print(jose.sobrenome)
